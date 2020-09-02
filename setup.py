5fd8e13ffa0f6a6140ba0b951e5e065a27386d55
Orlando Tomas <orlando.tomas@ergon.ch>
2020-09-02 22:29:19 +0200

    rename script

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 from version import __version__
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     readme = fh.read()
 
-with open('LICENSE') as f:
+with open('LICENSE', encoding='utf-8') as f:
     license = f.read()
 
 setuptools.setup(
@@ -13,15 +13,15 @@ setuptools.setup(
     author="Orlando Tomás",
     author_email="orlando.tomas@hotmail.com",
     url="https://github.com/orltom/git-history-tools",
-    # long_description=readme,
-    # long_description_content_type="text/markdown",
+    long_description=readme,
+    long_description_content_type='text/markdown',
     license=license,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
     ],
-    scripts=['cli.py'],
+    scripts=['githistory'],
     packages=['src'],
     python_requires='>=3.6',
 )
