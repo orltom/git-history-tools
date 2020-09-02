c62be42475fb5e7ff5c6cbe65691807e14148100
Orlando Tomás <orlando.tomas@ergon.ch>
2020-09-02 08:15:06 +0200

    Implement additional options

--- git-history-tool.py
+++ git-history-tool.py
@@ -2,20 +2,24 @@
 """git-history-tool.py
 
 Description:
-  Group GIT commits by user and show changed folders
+  Group GIT commits by user and show changes by
 
 Usage:
-  git-history-tool.py <path>
+  git-history-tool.py <path> [-b <name>] [-i <regex>] [-d <number>]
 
 Options:
-  -h --help         Show this screen.
-  <path>            GIT repository path
+  -h --help                     Show this screen.
+  -b --branch <name>            GIT branch name [default: master]
+  -i --issue <regex>            Regex pattern which correspond the issue ID. [default: ""]
+  -d --deep <number>            change file deep [default: 1]
+  <path>                        GIT repository path.
 """
 from datetime import datetime, timedelta
 from git import Repo
 from typing import List, Dict
 from tabulate import tabulate
 from docopt import docopt
+import re
 
 
 class Commit:
@@ -51,8 +55,10 @@ class Commit:
 
 
 class Summary:
-    def __init__(self):
+    def __init__(self, pattern, deep):
         self._commits = []
+        self._pattern = pattern
+        self._deep = deep
 
     def add(self, commit):
         self._commits.append(commit)
@@ -65,14 +71,18 @@ class Summary:
                 if count < 3:
                     _path = _f[0:_f.index('/')]
                 else:
-                    _path = '/'.join(_f.split("/", 3)[0:3])
+                    _path = '/'.join(_f.split("/", _deep)[0:_deep])
                 _paths.add(_path)
         return '\n'.join(_paths)
 
     def msg(self) -> str:
         _messages = set()
         for _c in self._commits:
-            _short_msg = _c.msg.split(" ")[0:1][0].replace(":", "")
+            match = self._pattern.search(_c.msg)
+            if match:
+                _short_msg = match.group(0)
+            else:
+                _short_msg = "NOT FOUND"
             _messages.add(_short_msg)
         return '\n'.join(_messages)
 
@@ -85,22 +95,22 @@ class Summary:
 
 class GitUtils:
     @staticmethod
-    def get_commits_from_yesterday(rep_path) -> List[Commit]:
+    def get_commits_from_yesterday(rep_path, branch) -> List[Commit]:
         start_date = (datetime.today() - timedelta(days=3)).strftime('%Y-%m-%d 23:59:59')
         end_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-        return GitUtils.get_commits_between(rep_path, start_date, end_date)
+        return GitUtils.get_commits_between(rep_path, branch, start_date, end_date)
 
     @staticmethod
-    def get_commits_between(rep_path, since, after) -> List[Commit]:
+    def get_commits_between(rep_path, branch, since, after) -> List[Commit]:
         _commits = []
         _repo = Repo(rep_path)
-        for _c in _repo.iter_commits('master', max_count=300, since=since, after=after):
+        for _c in _repo.iter_commits(branch, max_count=300, since=since, after=after):
             _commit = Commit(_c.hexsha, _c.author, _c.committed_datetime, _c.message, _c.stats.files)
             _commits.append(_commit)
         return _commits
 
     @staticmethod
-    def group_by_user(commits) -> Dict[str, Summary]:
+    def group_by_user(commits, pattern, deep) -> Dict[str, Summary]:
         _summaries_by_user = {}
         for _commit in commits:
             if _commit.author in _summaries_by_user:
@@ -108,21 +118,27 @@ class GitUtils:
                 _summary.add(_commit)
                 _summaries_by_user[_commit.author] = _summary
             else:
-                _summary = Summary()
+                _summary = Summary(pattern, deep)
                 _summary.add(_commit)
                 _summaries_by_user[_commit.author] = _summary
         return _summaries_by_user
 
 
-def show_changes_from_yesterday(_path):
-    commits = GitUtils.get_commits_from_yesterday(_path)
-    changes_by_user = GitUtils.group_by_user(commits)
-    table = [[key, changes_by_user[key].show(), changes_by_user[key].msg(), changes_by_user[key].hashes()] for key in changes_by_user]
-    headers = ["Autor", "Modules", "Message", "GIT hash"]
+def show_changes_from_yesterday(path, branch, pattern, deep):
+    commits = GitUtils.get_commits_from_yesterday(path, branch)
+    changes_by_user = GitUtils.group_by_user(commits, pattern, deep)
+    table = [
+        [key, changes_by_user[key].show(), changes_by_user[key].msg(), changes_by_user[key].hashes()]
+        for key in changes_by_user
+    ]
+    headers = ["Author", "Modules", "Issue", "GIT hash"]
     print(tabulate(table, headers=headers, tablefmt="fancy_grid"))
 
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-    path = arguments['<path>']
-    show_changes_from_yesterday(path)
+    _path = arguments['<path>']
+    _branch = arguments['--branch']
+    _issue_pattern = re.compile(arguments['--issue'])
+    _deep = int(arguments['--deep'])
+    show_changes_from_yesterday(_path, _branch, _issue_pattern, _deep)
